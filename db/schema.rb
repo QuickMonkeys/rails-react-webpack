07a952f52557dbd1748bc3e6de11849a9284a53f
carlosedb <cbarboza@gmail.com>
2016-09-28 11:48:20 +0000

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160928001549) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "languages", force: :cascade do |t|
     t.integer  "year"
     t.string   "name"
